6f09128551d402644cd1d51262e05c3661a1843b
Abdul <AMalik@lcs.on.ca>
2022-05-10 23:51:04 -0400

    Initial Commit

---
+++ CulminatingTaskKinesiology/CulminatingTaskKinesiologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CulminatingTaskKinesiologyApp.swift
+//  CulminatingTaskKinesiology
+//
+//  Created by Abdul Malik on 2022-05-10.
+//
+
+import SwiftUI
+
+@main
+struct CulminatingTaskKinesiologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
