a87120bb2e126caf7fefd5ffff123bb8a6ba4113
Abdul <AMalik@lcs.on.ca>
2022-05-11 10:06:08 -0400

    Added WrongAnswerView and a Statistic View

---
+++ CulminatingTaskKinesiology/Model/View/StatisticView.swift
@@ -0,0 +1,26 @@
+//
+//  StatisticView.swift
+//  CulminatingTaskKinesiology
+//
+//  Created by Abdul Malik on 2022-05-11.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    
+    // MARK: Stored properties
+    
+    
+    
+    // MARK: Computed properties
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StatisticView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatisticView()
+    }
+}
---
+++ CulminatingTaskKinesiology/Model/View/WrongAnswerView.swift
@@ -0,0 +1,26 @@
+//
+//  WrongAnswerView.swift
+//  CulminatingTaskKinesiology
+//
+//  Created by Abdul Malik on 2022-05-11.
+//
+
+import SwiftUI
+
+struct WrongAnswerView: View {
+    
+    // MARK: Stored properties
+    
+    
+    
+    // MARK: Computed properties
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WrongAnswerView_Previews: PreviewProvider {
+    static var previews: some View {
+        WrongAnswerView()
+    }
+}
